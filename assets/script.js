5d74ab83570757ff9f8c34a5951682cc181689bc
Vinita Yadav <yadav.vinita@gmail.com>
2023-02-04 23:31:32 +0000

    display todays date alongside cityname

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ $('#search-form').on('submit', function(event){
     event.preventDefault();
     var cityName = $('#search-input').val()
     console.log(cityName);
-    // $('#today').text(cityName);
     //
     const queryUrlGeo = "http://api.openweathermap.org/geo/1.0/direct?q=" +cityName+ "&appid=" + key;
 
@@ -17,19 +16,38 @@ $('#search-form').on('submit', function(event){
         url: queryUrlGeo,
         method: "GET"
     }).then(function(response){
-        
+        const city = $('<h2>');
+        city.text(response[0].name);
+        $('#today').append(city);
+        const dateToday = $('<span>');
+        dateToday.text(today);
+        city.append(' (' + today +')');
         const lat = response[0].lat
         const long = response[0].lon
         //build queryUrl to fetch weather forcast
-        const queryUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&" +"lon=" + long + "&appid=" + key;
+        const queryUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&" +"lon=" + long + "&units=metric&appid=" + key;
 
     $.ajax({
         url: queryUrl,
         method: "GET"
     }).then(function(response){
     console.log(response);
-    const temp = response.list[0].main.temp
-    console.log(temp);
+
+    
+    //dispplay temp, wind and humidity
+    const tempDiv = $('<div>');
+    tempDiv.text("Temp: " + response.list[0].main.temp + " °C");
+    $('#today').append(tempDiv);
+    const windDiv = $('<div>');
+    windDiv.text("Wind: " + response.list[0].wind.speed + " KPH");
+    $('#today').append(windDiv);
+    const humidityDiv = $('<div>');
+    humidityDiv.text("Humidity: " + response.list[0].main.humidity + " %");
+    $('#today').append(humidityDiv);
+    const icon = $('<span>');
+    icon.text(response.list[0].weather[0].icon);
+    
+
 
     })
 });
